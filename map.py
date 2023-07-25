eb9d42a761df7ece6d5e106a9ef5993f803fcb60
Jeff <rjhughes42@gmail.com>
2023-07-25 10:50:13 -0400

    added volcanoes text andtheir points on the map

--- map.py
+++ map.py
@@ -1,12 +1,29 @@
 import folium
+import os
+
 map = folium.Map(location=[38.58,-90.09], zoom_start=6, tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
 
-coord_list = [[45,-88],[25,-69],[47,-119],[26,-97]]
+coord_list = []
+vol_name = []
 
-for coord in coord_list:
-    fg.add_child(folium.Marker(location=coord, icon=folium.Icon(color='green')))
+with open ("Volcanoes.txt","r") as file:
+    for line in file:
+        new_vols = line.strip().split(',')
+        coord_list.append(new_vols[8:10])
+        vol_name.append(new_vols[2])
+
+coord_list.pop(0)
+vol_name.pop(0)
+
+
+
+
+    
+
+for coord,vol in zip(coord_list,vol_name):
+        fg.add_child(folium.Marker(location=coord, popup=vol, icon=folium.Icon(color='green')))
 
 map.add_child(fg)
 
