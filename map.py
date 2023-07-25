47a9f944f484c8bbd15ff710dd61cf6b4bf63e33
Jeff <rjhughes42@gmail.com>
2023-07-25 13:12:05 -0400

    Changed population layer to dynamic color based on pop.

--- map.py
+++ map.py
@@ -29,7 +29,8 @@ for lt,ln,vn,vs,ve,vt in zip(lat,lon,vol_name,vol_status,vol_elev,vol_type):
     
     fg.add_child(folium.CircleMarker(location=[lt,ln], radius=6, popup= "Name: {}\nStatus: {}\nElevation: {}m \nType: {}".format(vn,vs,ve,vt), fill_color=elev_color(ve), color="gray", fill_opacity=0.7))
 
-fg.add_child(folium.GeoJson(data=(open('world.json','r', encoding='utf-8-sig').read())))    
+fg.add_child(folium.GeoJson(data=open('world.json','r', encoding='utf-8-sig').read(),
+style_function= lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000 else 'orange' if 10000000 <= x['properties']['POP2005']< 20000000 else 'red'}))    
 map.add_child(fg)
 
 map.save("Map1.html")
