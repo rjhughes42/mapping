82168f17f05ab44a1d10abee9acc37bbb10798f5
Jeff <rjhughes42@gmail.com>
2023-07-25 12:51:10 -0400

    Added descriptive information in popup with dynamic color for elevation

--- map.py
+++ map.py
@@ -1,30 +1,33 @@
 import folium
 import os
+import pandas
+
+def elev_color(elev):
+    if elev < 1000:
+        return 'green'
+    elif 1000<= elev < 3000:
+        return 'orange'
+    else:
+        return 'red'
 
 map = folium.Map(location=[38.58,-90.09], zoom_start=6, tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
 
-coord_list = []
-vol_name = []
 
-with open ("Volcanoes.txt","r") as file:
-    for line in file:
-        new_vols = line.strip().split(',')
-        coord_list.append(new_vols[8:10])
-        vol_name.append(new_vols[2])
-
-coord_list.pop(0)
-vol_name.pop(0)
+data = pandas.read_csv("Volcanoes.txt")
+vol_name = list(data["NAME"])
+vol_status = list(data["STATUS"])
+vol_elev = list(data["ELEV"])
+vol_type = list(data["TYPE"])
+lon = list(data["LON"])
+lat = list(data["LAT"])
 
 
 
-
+for lt,ln,vn,vs,ve,vt in zip(lat,lon,vol_name,vol_status,vol_elev,vol_type):
     
-
-for coord,vol in zip(coord_list,vol_name):
-        fg.add_child(folium.Marker(location=coord, popup=vol, icon=folium.Icon(color='green')))
-
+    fg.add_child(folium.CircleMarker(location=[lt,ln], radius=6, popup= "Name: {}\nStatus: {}\nElevation: {}m \nType: {}".format(vn,vs,ve,vt), fill_color=elev_color(ve), color="gray", fill_opacity=0.7))
 map.add_child(fg)
 
 map.save("Map1.html")
